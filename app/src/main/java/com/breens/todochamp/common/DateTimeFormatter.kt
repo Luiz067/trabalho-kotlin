74f2ffc13df4e57b2eab0be151573a7b0be494bd
heroshg <heros.goodoy@gmail.com>
2024-11-19 20:52:26 -0300

    portugues

--- app/src/main/java/com/breens/todochamp/common/DateTimeFormatter.kt
+++ app/src/main/java/com/breens/todochamp/common/DateTimeFormatter.kt
@@ -4,17 +4,17 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
+// Retorna a data atual como string
 fun getCurrentTimeAsString(): String {
     val currentTime = Date()
-    val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
-    val currentTimeString = dateFormat.format(currentTime)
-    return currentTimeString
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale("pt", "BR")) // Altera para português
+    return dateFormat.format(currentTime)
 }
 
+// Converte uma string de data para um formato legível em português
 fun convertDateFormat(dateString: String): String {
-    val currentDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
-    val newDateFormat = SimpleDateFormat("EEEE MMMM yyyy, h:mma", Locale.getDefault())
+    val currentDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale("pt", "BR")) // Define o formato original em português
+    val newDateFormat = SimpleDateFormat("EEEE, d 'de' MMMM 'de' yyyy, HH:mm", Locale("pt", "BR")) // Formato novo em português
     val date = currentDateFormat.parse(dateString)
-    val newDateString = newDateFormat.format(date)
-    return newDateString
+    return newDateFormat.format(date!!)
 }
